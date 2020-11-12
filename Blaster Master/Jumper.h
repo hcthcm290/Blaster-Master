bf1aa266861ad99b03cc5ba3db94f6ce638f3405
Thành Trung Nguyễn <trungbadaocm@gmail.com>
2020-11-12 22:05:19 +0700

    Update behavior for Jumper, hit wall flip back, fix small bug that only I know

--- Blaster Master/Jumper.h
+++ Blaster Master/Jumper.h
@@ -2,7 +2,7 @@
 #include "DynamicObject.h"
 #include "CollisionSystem.h"
 #include "PlayScene.h"
-#define jumperSpeed	10;
+#define jumperSpeed	30;
 #define jumperWalk State::_JUMPER_WALK_
 #define	jumperIdle State::_JUMPER_IDLE_
 
@@ -17,7 +17,7 @@ private:
 	float waitForJump = 2;
 	int direction = 1;
 	int jumpCount=0;
-	float mini_waitForJump = 0.1;
+	float mini_waitForJump = 0;
 	float accelerateY = 700;
 	float jumpTime;
 public:
