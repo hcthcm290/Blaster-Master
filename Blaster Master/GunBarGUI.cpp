914a9ba2cb5186545e41d62941ba4425b3738b74
hcthcm290 <dragonnica123@gmail.com>
2020-12-09 10:46:36 +0700

    Fix Exception with Gunbar when render is call before the update when start the scene

--- Blaster Master/GunBarGUI.cpp
+++ Blaster Master/GunBarGUI.cpp
@@ -1,6 +1,7 @@
 #include "GunBarGUI.h"
 #include "TheEye.h"
 #include "Camera.h"
+#include "Debug.h"
 
 GunBarGUI::GunBarGUI()
 {
@@ -11,11 +12,21 @@ GunBarGUI::GunBarGUI()
 void GunBarGUI::Update(float dt)
 {
 	gunPower = TheEye::GetInstance()->GetBigJason()->getGunPower();
+
+	if (gunPower < 1 || gunPower > 8)
+	{
+		DebugOut(L"Error with gunpower bar");
+	}
 }
 
 void GunBarGUI::Render()
 {
 	D3DXVECTOR3 cameraPosition = Camera::GetInstance()->GetPosition();
+
+	if (gunPower < 1 || gunPower > 8)
+	{
+		gunPower = TheEye::GetInstance()->GetBigJason()->getGunPower();
+	}
 
 	// we need -1 because gunpower start at lv 1
 	CSprites::GetInstance()->Get(16200 + gunPower - 1)->Draw(cameraPosition.x + offset.x, cameraPosition.y + offset.y);
